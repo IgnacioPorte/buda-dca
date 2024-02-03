61ce8c23c77946ddec0c3ecdc7b1451560985fba
Ignacio Porte <ignacio.porte@uc.cl>
2024-02-02 21:26:17 -0300

    feat(components): add chart options components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Bitcoin from "@/assets/bitcoin.png";
+import { Input } from "@/components/ui/input";
+import { DatePicker } from "@/components/ui/datePicker";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <img src={Bitcoin} alt="Bitcoin" className="animate-spin w-20 h-20 mx-auto my-10" />
+
+      <h1 className="text-4xl font-bold text-center">DCA Crypto Calculadora</h1>
+      <h2 className="text-2xl text-center my-10">
+        Rendimiento histórico del DCA al comprar Bitcoin (BTC) mensualmente con
+        2 US Dollar durante los últimos 1095 días.
+      </h2>
+      <div>{/* dinero invertido */}</div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="my-10 w-1/5 flex flex-col gap-2">
+          <h3 className="text-lg  text-left">Criptomoneda</h3>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Bitcoin" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Bitcoin</SelectItem>
+              <SelectItem value="dark">Ethereum</SelectItem>
+              <SelectItem value="system">Litecoin</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <div className="flex flex-col gap-2">
+            <h3 className="text-lg  text-left">Cantidad</h3>
+            <div className="flex flex-row">
+              <Input type="number" placeholder="0.00" />
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="USD" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">USD</SelectItem>
+                  <SelectItem value="dark">EUR</SelectItem>
+                  <SelectItem value="system">MXN</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <h3 className="text-lg  text-left">Frecuencia</h3>
+
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Mensual" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Mensual</SelectItem>
+              <SelectItem value="dark">Semanal</SelectItem>
+              <SelectItem value="system">Diario</SelectItem>
+            </SelectContent>
+          </Select>
+
+          <h3 className="text-lg  text-left">Fecha de inicio</h3>
+
+          <DatePicker />
+
+          <h3 className="text-lg  text-left">Fecha de fin</h3>
+          <DatePicker />
+        </div>
+        <div className="w-1/2">{/* chart */}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
