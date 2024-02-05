a1bc329319a4e0edbd24e80dc101cc44b3e163d8
Ignacio Porte <ignacio.porte@uc.cl>
2024-02-04 23:03:26 -0300

    chore(docker): dockerize app

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Bun image
+FROM oven/bun:latest as base
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy the entire project into the container
+COPY . .
+
+# Install dependencies
+RUN bun install
+
+# Build the app
+RUN bun run build
+
+# Expose the port
+EXPOSE 8080
+
+# Use a shell command to run both the proxy and the app concurrently
+CMD sh -c "bun run proxy & bun run preview"
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+  },
+});
