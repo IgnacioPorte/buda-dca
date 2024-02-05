93a9ad27897077cf6d93fef54a61924cc2f2e65c
Ignacio Porte <ignacio.porte@uc.cl>
2024-02-04 22:04:52 -0300

    feat(api): add axios client with buda rateLimits

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import rateLimit from "axios-rate-limit";
+
+const api = rateLimit(
+  axios.create({
+    baseURL: "http://localhost:8010/proxy/api/v2",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }),
+  { maxRequests: 120, perMilliseconds:  60 * 1000 }
+);
+
+export default api;
